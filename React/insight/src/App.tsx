866ce0ff7ee47a565b25dfee7b68630641f45190
Karabelo Nthoroane <karabelonthoroane@gmail.com>
2024-08-01 16:05:04 +0200

    Fetching external data api

--- React/insight/src/App.tsx
+++ React/insight/src/App.tsx
@@ -1,13 +1,91 @@
-import './App.css'
-import NewsFeed from './components/NewsFeed'
+import NewsFeed from "./components/NewsFeed";
+import { useState } from "react";
 
-function App() { 
+// Bootstrap CSS and JS
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+function App() {
+  // Determine the selected news category
+  const [newsCategory, setCategory] = useState("everything");
+
+  const selectedCategory = (category: string) => {
+    switch (category) {
+      case "Business":
+        setCategory("business");
+        break;
+
+      case "Sports":
+        setCategory("sports");
+        break;
+
+      case "Politics":
+        setCategory("politics");
+        break;
+
+      case "Technology":
+        setCategory("technology");
+        break;
+      default:
+        setCategory("everything");
+        break;
+    }
+  };
 
   return (
-    <>
-    <NewsFeed />
-    </>
-  )
+    <div className="container-sm">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary border-bottom mb-4">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            News Feed
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" onClick={() => selectedCategory("")}>
+                  All
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" onClick={() => selectedCategory("Business")}>
+                  Business
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" onClick={() => selectedCategory("Sports")}>
+                  Sports
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" onClick={() => selectedCategory("Politics")}>
+                  Politics
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" onClick={() => selectedCategory("Technology")}>
+                  Technology
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <p>Selected category: {newsCategory}</p>
+      <NewsFeed />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- React/insight/src/components/NewsFeed.tsx
+++ React/insight/src/components/NewsFeed.tsx
@@ -3,12 +3,18 @@ import axios from "axios";
 
 function NewsFeed() {
   const [news, setNews] = useState([]);
+  const apiKey: string = "9a45d8a7572c4bbeb1cd2e1bd99ae90c";
+  const country: string = "za";
+  const category: string = "entertainment";
 
   const fetchNews = async () => {
     try {
+      // Use template literals for string interpolation
       const feed = await axios.get(
-        "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=9a45d8a7572c4bbeb1cd2e1bd99ae90c"
+        `https://newsapi.org/v2/everything?domains=wsj.com&apiKey=${apiKey}`
       );
+
+      console.log(feed);
 
       setNews(feed.data.articles);
     } catch (error) {
@@ -21,18 +27,48 @@ function NewsFeed() {
   }, []);
 
   return (
-    <div>
-      <h1>News Feed</h1>
-      {news.map((article, index) => (
-        <div key={index}>
-          <h2>{article.title}</h2>
-          <p>{article.author}</p>
-          <p>{article.description}</p>
-          <a href={article.url} target="_blank" rel="noopener noreferrer">
-            Read more...
-          </a>
+    <div className="container flex news-feed">
+      <div className="d-flex justify-content-between">
+        <div className="row">
+          {news.map((article, index) => (
+            <div className="col-sm-6">
+              <div className="card mb-4">
+                <div></div>
+
+                {/* Card Image */}
+                <img
+                  className="card-img-top"
+                  src={article.urlToImage}
+                  alt="Articles"
+                />
+
+                <div className="card-body">
+                  {/* Article title */}
+                  <h5 className="card-header">{article.title}</h5>
+
+                  {/* Article summary */}
+                  <p className="card-text m-2">{article.description}</p>
+
+                  {/* Article author */}
+                  <p className="card-subtitle m-2 text-body-secondary">
+                    {article.author}
+                  </p>
+
+                  {/* Read More Link */}
+                  <a
+                    className="btn btn-primary"
+                    href={article.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Read more...
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 }
